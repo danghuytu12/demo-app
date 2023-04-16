ef143b693447de8dc12305f3ebf1cc488d763b91
huytu20 <danghuytu.thd2006@gmail.com>
2023-04-16 09:15:10 +0700

    config

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const test = process.env.REACT_APP_API_PUBLIC
 
 function App() {
+  const [data, setData] = useState<any>()
   useEffect(() => {
-    axios.get(`${test}/todos/1`).then((res) => console.log(res))
+    axios.get(`${test}/todos/1`).then((res) => setData(res.data))
   }, [])
+  console.log(data)
   return (
     <div className="App">
       <header className="App-header">
-
+        <p>UserID: {data.id}</p>
+        <p>Completed: {data.completed}</p>
+        <p>Title: {data.title}</p>
+        <p>ID: {data.userId}</p>
       </header>
     </div>
   );
